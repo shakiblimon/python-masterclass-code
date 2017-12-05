f4b58cd2899225043c15c80f8b1586d53a9caa32
shakib <shakibcselimon@gmail.com>
2017-12-06 00:27:42 +0600

    Adding oop concepts class and object

--- masterclass_quiz.py
+++ masterclass_quiz.py
@@ -1,3 +1,4 @@
+'''
 #       Calculating the interest
 ####################################
 p = int(input('Enter Value of p : '))
@@ -259,4 +260,26 @@ def even(x):
             yield i
 print(list(even(20)))
 
+'''
+#############################################
+#       Object Oriented Python
+#############################################
+class Students:         #Craeting class
+    def __init__(self,name,contact):     # Initaiting class propertices
+        self.name= name
+        self.contact = contact
+
+
+
+    def getdate(self):
+        print(' Raedy to Accept Data')
+        self.name = input('Enter Name : ')
+        self.contact = input('Enter Contact: ')
+
+    def putdata(self):
+        print('The name is  : '+self.name, '\nThis is  the contact: '+self.contact)
+
+shakib = Students('blank',0)
+shakib.getdate()
+shakib.putdata()
 
