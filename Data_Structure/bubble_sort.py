f40234a2ce7d093a2bd741468b331d391a4b8230
Shakib Limon <shakib.limon@circlefintech.com>
2019-06-13 13:31:50 +0600

    bubble sort

--- Data_Structure/bubble_sort.py
+++ Data_Structure/bubble_sort.py
@@ -1,8 +1,13 @@
 def bubble_sort(a):
+    """
+    :param a:
+    :return:
+    """
     for i in range(0, len(a)):
         for j in range (0, len(a)-i-1):
             if (a[j]>a[j+1]):
                 a[j],a[j+1] = a[j+1], a[j]
+
 
 if __name__ == '__main__':
     a = [2,9,7,17,10,8]
